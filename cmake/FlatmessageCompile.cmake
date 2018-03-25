fc0e35039becde78c29c12c2348c493a9e598439
DennisWG <DennisWG@users.noreply.github.com>
2018-03-25 17:09:00 +0200

    Updated CMakeLists

--- cmake/FlatmessageCompile.cmake
+++ cmake/FlatmessageCompile.cmake
@@ -1,4 +1,4 @@
-#   Copyright 2017 KeycapEmu
+#   Copyright 2018 KeycapEmu
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -13,16 +13,24 @@
 #   limitations under the License.
 
 
-# Generates the given fileName witht he flatmessage compiler
-function(flatmessage_compile_file fileName)
+# Generates the given fileName with the flatmessage compiler
+function(flatmessage_compile_file fileName template fileExtension)
     execute_process(
-        COMMAND flatmessage_compiler -i ${CMAKE_CURRENT_SOURCE_DIR}/${fileName} -t ${PROJECT_SOURCE_DIR}/templates/hpp.template -e hpp -o ${CMAKE_CURRENT_BINARY_DIR}
+        COMMAND flatmessage_compiler -i ${CMAKE_CURRENT_SOURCE_DIR}/${fileName} -t ${template} -e ${fileExtension} -o ${CMAKE_CURRENT_BINARY_DIR}/..
         OUTPUT_VARIABLE PROCESSOR_OUTPUT
         ERROR_VARIABLE PROCESSOR_ERROR
         RESULT_VARIABLE PROCESSOR_RET
     )
 
     if(PROCESSOR_RET)
-        message(FATAL_ERROR "Error calling code generator with \"\n" "-i ${CMAKE_CURRENT_SOURCE_DIR}/${fileName} -t ${PROJECT_SOURCE_DIR}/templates/hpp.template -e hpp -o ${CMAKE_CURRENT_BINARY_DIR}\"\n" ${PROCESSOR_RET})
+        message(FATAL_ERROR "Error calling code generator with \"\n" "-i ${CMAKE_CURRENT_SOURCE_DIR}/${fileName} -t ${template} -e hpp -o ${CMAKE_CURRENT_BINARY_DIR}/..\"\n" ${PROCESSOR_RET} " - " ${PROCESSOR_ERROR})
     endif()
+endfunction()
+
+function(flatmessage_compile_files)
+    set(oneValueArgs TEMPLATE EXTENSION)
+    cmake_parse_arguments(PARSED_ARGS "" "${oneValueArgs}" "FILES" ${ARGN})
+    foreach(file ${PARSED_ARGS_FILES})
+        flatmessage_compile_file(${file} ${PARSED_ARGS_TEMPLATE} ${PARSED_ARGS_EXTENSION})
+    endforeach()
 endfunction()
