78490a31c3fe31448a7f5757ee224f958af43c8b
DennisWG <DennisWG@users.noreply.github.com>
2018-06-24 12:05:13 +0200

    Cli/Handler: Fully implemented support for nested commands

--- src/Shared/Cli/Handler.cpp
+++ src/Shared/Cli/Handler.cpp
@@ -17,20 +17,74 @@
 #include "Handler.hpp"
 #include "../Rbac/Role.hpp"
 
+#include <spdlog/fmt/fmt.h>
+
+#include <algorithm>
+#include <iostream>
+
+bool isChild(std::vector<std::string> const& arguments, Keycap::Shared::Cli::Command const& command)
+{
+    // clang-format off
+    return std::any_of(std::begin(command.childCommands), std::end(command.childCommands), [&](auto&& value)
+    {
+        return std::any_of(std::begin(arguments), std::end(arguments), [&](auto&& argument)
+        {
+            return argument == value.name;
+        });
+    });
+    // clang-format on
+}
+
 namespace Keycap::Shared::Cli
 {
+    HandlerResult DoCommand(Command const& command, std::vector<std::string> const& arguments, Rbac::Role const& role)
+    {
+        if (!role.Has(command.permission))
+            return HandlerResult::InsufficientPermissions;
+
+        if (command.handler)
+            return command.handler(arguments, role) ? HandlerResult::Ok : HandlerResult::CommandFailed;
+
+        std::cout << "Sub commands (name - required permission - description):\n";
+        for (auto&& child : command.childCommands)
+            std::cout << fmt::format("{} - {} - {}\n", child.name, child.permission.ToString(), child.description);
+
+        return HandlerResult::Ok;
+    }
+
+    HandlerResult HandleChildCommand(std::string const& name, std::vector<std::string> const& arguments,
+                                     Rbac::Role const& role, Command const& parent);
+
+    HandlerResult HandleCommand(Command const& command, std::string const& name,
+                                std::vector<std::string> const& arguments, Rbac::Role const& role)
+    {
+        if (!isChild(arguments, command))
+            return DoCommand(command, arguments, role);
+
+        auto newName = arguments[0];
+        auto newArguments = std::vector<std::string>{std::begin(arguments) + 1, std::end(arguments)};
+
+        return HandleChildCommand(newName, newArguments, role, command);
+    }
+
+    HandlerResult HandleChildCommand(std::string const& name, std::vector<std::string> const& arguments,
+                                     Rbac::Role const& role, Command const& parent)
+    {
+        auto& childen = parent.childCommands;
+        auto command
+            = std::find_if(std::begin(childen), std::end(childen), [&](auto&& child) { return child.name == name; });
+
+        if (command != childen.end())
+            return HandleCommand(*command, name, arguments, role);
+
+        return HandlerResult::CommandNotFound;
+    }
+
     HandlerResult HandleCommand(std::string const& name, std::vector<std::string> const& arguments,
                                 Rbac::Role const& role, CommandMap const& commands)
     {
         if (auto itr = commands.find(name); itr != commands.end())
-        {
-            auto& command = itr->second;
-
-            if (!role.Has(command.permission))
-                return HandlerResult::InsufficientPermissions;
-
-            return command.handler(arguments, role) ? HandlerResult::Ok : HandlerResult::CommandFailed;
-        }
+            return HandleCommand(itr->second, name, arguments, role);
 
         return HandlerResult::CommandNotFound;
     }
