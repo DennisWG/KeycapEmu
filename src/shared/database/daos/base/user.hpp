4907a9002583eda87eb066b3449f77a7be6f78e1
DennisWG <DennisWG@users.noreply.github.com>
2019-01-03 02:37:18 +0100

    It's the current year!

--- src/shared/database/daos/base/user.hpp
+++ src/shared/database/daos/base/user.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2018 KeycapEmu
+    Copyright 2018-2019 KeycapEmu
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
